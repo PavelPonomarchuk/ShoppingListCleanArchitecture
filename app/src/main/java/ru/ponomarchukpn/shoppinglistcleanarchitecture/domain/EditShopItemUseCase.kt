3235878aa1bdf30091dc8804be02c41e568dbdbe
PavelPonomarchuk <ponomarchuk83@gmail.com>
2023-02-20 19:16:34 +0500

    Repository interface has been added

--- app/src/main/java/ru/ponomarchukpn/shoppinglistcleanarchitecture/domain/EditShopItemUseCase.kt
+++ app/src/main/java/ru/ponomarchukpn/shoppinglistcleanarchitecture/domain/EditShopItemUseCase.kt
@@ -1,8 +1,8 @@
 package ru.ponomarchukpn.shoppinglistcleanarchitecture.domain
 
-class EditShopItemUseCase {
+class EditShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
     fun editShopItem(shopItem: ShopItem) {
-
+        shopListRepository.editShopItem(shopItem)
     }
 }
