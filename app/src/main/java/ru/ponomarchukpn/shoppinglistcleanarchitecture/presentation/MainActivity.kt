0749510da2ec31f6cb94a49af43746cdc2ae86b0
PavelPonomarchuk <ponomarchuk83@gmail.com>
2023-02-23 13:35:25 +0500

    ViewModel has been added

--- app/src/main/java/ru/ponomarchukpn/shoppinglistcleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/ru/ponomarchukpn/shoppinglistcleanarchitecture/presentation/MainActivity.kt
@@ -2,11 +2,20 @@ package ru.ponomarchukpn.shoppinglistcleanarchitecture.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import ru.ponomarchukpn.shoppinglistcleanarchitecture.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+
+        }
+        viewModel.getShopList()
     }
 }
